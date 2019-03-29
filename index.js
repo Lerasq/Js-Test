0e1201b80e86fbba07e528f0c7d34dfd97117683
Lerasq <santi.castillo.ferrandis@gmail.com>
2019-03-29 09:00:36 +0100

    2 files modified

--- index.js
+++ index.js
@@ -1,6 +1,13 @@
+"use strict"
 // Import stylesheets
 import './style.css';
 
 // Write Javascript code!
+const pi = 3.1416;
+const RED = "#534212";
+let cacatua;
+cacatua = 15.4;
+let cotorra = 2.43;
 const appDiv = document.getElementById('app');
 appDiv.innerHTML = `<h1>JS Starter</h1>`;
+document.getElementById(cotorra);
