b01fdd942918132feb45460955b11f3bb47482ea
Lerasq <santi.castillo.ferrandis@gmail.com>
2019-03-29 09:12:17 +0100

    1 file modified

--- index.js
+++ index.js
@@ -1,13 +1,16 @@
+//This strict mode is for be correct with the variables and the writes of Javascript. It makes compatible with old versions and is the best mode for the good practises.
 "use strict"
-// Import stylesheets
+// Import stylesheets because we are working on Javascript you know!.
 import './style.css';
-
-// Write Javascript code!
-const pi = 3.1416;
-const RED = "#534212";
-let cacatua;
-cacatua = 15.4;
-let cotorra = 2.43;
-const appDiv = document.getElementById('app');
-appDiv.innerHTML = `<h1>JS Starter</h1>`;
+// Javascript just testing costants and variables. (let) - javascript.info
+let admin, 
+    name;
+name = "John";
+admin = name;
+//Javascript serius for learn about it and do it right!.
+const PLANET = "Earth";
+const userVisitor;
+/*const appDiv = document.getElementById('app');
+appDiv.innerHTML = `<h1>JS Starter</h1>`;*/
 document.getElementById(cotorra);
+alert(admin);
